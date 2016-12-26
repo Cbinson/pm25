e52cb52071544f6cf6278db6861d417382255e93
binsonchang <binsonchang@senao-mbpr-adison.local>
2016-12-26 15:30:27 +0800

    add package

--- pm.js
+++ pm.js
@@ -52,6 +52,23 @@ var pm25 = function() {
   });
 };
 
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+
 //連接db
 mongoose.connect('mongodb://binson:binsonpm25@ds141098.mlab.com:41098/airpm');
 
